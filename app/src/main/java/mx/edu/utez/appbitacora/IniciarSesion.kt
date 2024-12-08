01ec7f23d6434d6a1cccc2c2c4fc44bfe1fc2d61
El_Actor <20233tn075@utez.edu.mx>
2024-12-07 18:32:20 -0600

    Funcion inisio de sesion

--- app/src/main/java/mx/edu/utez/appbitacora/IniciarSesion.kt
+++ app/src/main/java/mx/edu/utez/appbitacora/IniciarSesion.kt
@@ -1,20 +1,69 @@
 package mx.edu.utez.appbitacora
 
+import android.app.DownloadManager.Request
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import mx.edu.utez.appbitacora.admin.MenuAdmin
+import mx.edu.utez.appbitacora.databinding.ActivityIniciarSesionBinding
+import mx.edu.utez.appbitacora.registro.InicioRegistro
+import org.json.JSONObject
 
 class IniciarSesion : AppCompatActivity() {
+    private lateinit var binding : ActivityIniciarSesionBinding
+    private var user = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_iniciar_sesion)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityIniciarSesionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnIniciar.setOnClickListener{
+            val email = binding.edtEmailInc.text
+            val pass = binding.editTextTextPassword.text
+            login(email.toString(), pass.toString())
+
         }
+
+    }
+
+    fun login(email : String, password : String){
+        val queue = Volley.newRequestQueue(this)
+        val metodo = com.android.volley.Request.Method.POST
+        val url = "http://192.168.100.5:8080/api/login?correo=${email}@utez.edu.mx&contrasenia=${password}"
+        val listener = Response.Listener<JSONObject> { r ->
+            user = r.getJSONObject("usuarioResponse").getJSONArray("usuario").getJSONObject(0).getString("rol")
+            if(!user.isNullOrEmpty()){
+                Toast.makeText(this,"Bienvenido "+"${r.getJSONObject("usuarioResponse").getJSONArray("usuario").getJSONObject(0).getString("nombre")}",
+                    Toast.LENGTH_LONG).show()
+            }
+            if(user.equals("Alumno")){
+                val intnet = Intent(this, InicioRegistro::class.java)
+                intnet.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intnet)
+            }else if (user.equals("Admin")){
+                val intnet = Intent(this, MenuAdmin::class.java)
+                intnet.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intnet)
+            }
+        }
+
+        val errorListener = Response.ErrorListener { e ->
+            Log.e("ErrorLogin", e.message.toString())
+        }
+
+        val request = JsonObjectRequest(metodo, url, null, listener, errorListener)
+        queue.add(request)
+
+
+
+
     }
 }
